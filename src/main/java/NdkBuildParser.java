d2879828ca8a096ed6187a123a514a49f2a32909
Jomo Fisher <jomof@google.com>
2015-12-11 13:02:09 -0800

    Initial commit

---
+++ src/main/java/NdkBuildParser.java
@@ -0,0 +1,11 @@
+package com.jomof.ndkbuildparser;
+
+/**
+ * Created by jomof on 12/11/15.
+ */
+public class NdkBuildParser {
+
+    public static void main(String arg[]) {
+        System.out.printf("Hello world\n");
+    }
+}
