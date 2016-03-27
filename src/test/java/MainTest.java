a71df8165f4795dcfb537cb146aa4590f9422f8d
Jomo Fisher <jomofisher@gmail.com>
2016-03-27 11:34:16 -0700

    Initial

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,22 +1,10 @@
-import com.google.NdkBuildToAndroidStudio.Main;
 import org.junit.Test;
-
-import java.io.IOException;
 
 /**
  * Created by jomof on 1/15/16.
  */
 public class MainTest {
-    private void checkFlow(String json, int expectedHashCode) throws IOException {
-        if (expectedHashCode != json.toString().hashCode()) {
-            throw new RuntimeException(
-                    String.format("Expected hashCode '%s' but got '%s '%s'", expectedHashCode, json.hashCode(), json));
-        }
-    }
-
     @Test
-    public void teapotBasic() throws IOException, InterruptedException {
-        String result = Main.ndkBuildToJson("support-files/android-ndk-r10e/samples/Teapot");
-        checkFlow(result, -496724433);
+    public void test1() {
     }
 }
